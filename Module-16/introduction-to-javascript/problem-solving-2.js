8b91da940921ca2a7748f0a9216c3586fa7aeabc
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-07-31 15:32:35 +0600

    problem solving 2 done

---
+++ Module-16/introduction-to-javascript/problem-solving-2.js
@@ -0,0 +1,12 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks = mathematics + biology + chemistry + physics + bangla;
+var totalSubjecs = 5;
+var all = totalMarks / totalSubjecs;
+all = all.toFixed(2);
+console.log(all);
+
