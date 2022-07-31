8ad4609f7b0b5f9797de12055728aa31c1682333
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-07-31 11:39:07 +0600

    which types of name not used in variavble

---
+++ Module-16/introduction-to-javascript/variable-names.js
@@ -0,0 +1,36 @@
+var price = 29 // var is keyword
+
+
+/* 
+    What won't happen for javascript variable names?
+
+*/
+
+// a) Variable name can not be any keywords. like
+var false = 96 // Wrong
+var return = true // wrong
+
+// b) Variable name has to be in one word. No space.
+var my home address = 'dhaka' // wrong
+
+// c) Variable name can not have qotation
+var "name" = 'sazzad' // wrong
+
+// d) Variable name can not starts with a number but can ends with a number
+var 99club = 1964 // wrong
+var club99 = 1956 // Right
+
+// e) How to use long names & can not use dash
+var user-name = 'sazzad' // wrong
+var user_name = 'sazzad' // right - snakecase
+var userName = 'sazzad' // camelcase
+var Username = 'sazzad' // pascal case
+
+    // Keyword is case-sensetive
+    vaR sazzad // wrong
+
+// Variable name is case sensetive
+var xyz = 'apple';
+var XYZ = 'apple';
+
+
