fcc8ddd031816c9a399a84c31dec636cfcdea899
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-07-31 11:18:10 +0600

    how to decleare a variable

---
+++ Module-16/introduction-to-javascript/variable.js
@@ -0,0 +1,10 @@
+// vary + able = variable
+// Declearing a javascript variable requires 5 important things
+var price = 32;
+var age = 31;
+var temparature = 38;
+var weight = 80;
+var money = 100;
+var rent = 12000
+
+// Its all about numeric data type
