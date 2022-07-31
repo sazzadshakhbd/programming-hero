9255507a9b2196040b97f1d35f289e2af156f37c
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-07-31 15:03:22 +0600

    differnce between parInt & parseFloat

---
+++ Module-16/introduction-to-javascript/concatination.js
@@ -0,0 +1,11 @@
+var price1 = '31';
+var price2 = 10;
+
+// How to convert string-number
+var price3 = parseInt(price1);
+var totalPrice = price3 + price2;
+console.log(totalPrice);
+
+var gpa = '41.3'
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
