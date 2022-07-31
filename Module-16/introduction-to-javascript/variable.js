128e9c66a45116a929a8dc8d0d7da7deec0e68fe
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-07-31 11:24:12 +0600

    intro to string & bollean data type

--- Module-16/introduction-to-javascript/variable.js
+++ Module-16/introduction-to-javascript/variable.js
@@ -1,5 +1,7 @@
 // vary + able = variable
 // Declearing a javascript variable requires 5 important things
+
+// Its all about numeric data type
 var price = 32;
 var age = 31;
 var temparature = 38;
@@ -7,4 +9,15 @@ var weight = 80;
 var money = 100;
 var rent = 12000
 
-// Its all about numeric data type
+// Its all about string data types
+var person = "sazzad";
+var location = 'Dhaka';
+var special = "Adib";
+var serious = 'Family';
+var fullName = 'Md. Sazzad Ali Shakh';
+
+// Its all about boolean data-type
+var isSingle = false
+
+// What is variable? We can think variable as a box where we can store things
+
