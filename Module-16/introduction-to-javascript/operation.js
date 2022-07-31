4b9dc7e2dd98efb2779eaddf13bb1caac2f8d047
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-07-31 12:32:35 +0600

    how to decleare shortly

--- Module-16/introduction-to-javascript/operation.js
+++ Module-16/introduction-to-javascript/operation.js
@@ -4,5 +4,5 @@ var eggPrice = 10;
 //  var totalPrice = onionPrice + eggPrice; [Addition]
 //  var priceDifference = onionPrice - eggPrice; [Substruction]
 // var totalCount = onionPrice * eggPrice; [Multiplication]
-var result = onionPrice / eggPrice;
+// var result = onionPrice / eggPrice; [Division]
 console.log(result);
---
+++ Module-16/introduction-to-javascript/shorthand.js
@@ -0,0 +1,34 @@
+var tomatoPrice = 25; // Variable decleartion
+tomatoPrice = 30;// variable value update
+
+// Add something to the previous value
+var price1 = 31;
+var price2 = 10;
+
+// Set a new value
+price1 = 35;
+
+// Add some value to the existing value
+price1 = price1 + 10;
+
+// Addition to the same variable short hand
+price1 += 10;
+
+// Substraction to the same variable short hand
+price1 -= 20;
+
+// Multiplication to the same variable short hand
+price1 *= 2;
+
+// Division to the same variable short hand
+price1 /= 35;
+
+var age = 39;
+age += 1;
+age++; // ++ means addign 1 to the existing
+
+var love = 100;
+love -= 1;
+love--; // ++ means addign 1 to the existing
+console.log(love);
+
