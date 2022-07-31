3abf265097247cd77a128753d314ec53e084a581
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-07-31 11:12:31 +0600

    which terms learn for javascript as a beginner

---
+++ Module-16/introduction-to-javascript/math.js
@@ -0,0 +1,12 @@
+// Number: 55, 898, -32
+// Integer: 2,5, odd, even
+// Float: decimal-2.5,13.67,32.69
+// Operations: addition, substruction, multiplication, division, remainder
+// percent: 10%, 20%, 30%
+// Average:
+// Comparision: <, >, ==, !=, >=, <=
+// Unit Number (ঐকিক অংক )
+// max, min, avg, sum
+// Series:
+// Extra: a) Prime Number, Permutation,
+// if you need, you have to learn it
