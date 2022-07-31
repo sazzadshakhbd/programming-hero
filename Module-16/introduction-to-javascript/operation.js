f798f0da0b8764634c2ad7a199f110636eef92fd
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-07-31 11:46:54 +0600

    how to operate using addition,substriction,multiplication & division

---
+++ Module-16/introduction-to-javascript/operation.js
@@ -0,0 +1,8 @@
+
+var onionPrice = 20;
+var eggPrice = 10;
+//  var totalPrice = onionPrice + eggPrice; [Addition]
+//  var priceDifference = onionPrice - eggPrice; [Substruction]
+// var totalCount = onionPrice * eggPrice; [Multiplication]
+var result = onionPrice / eggPrice;
+console.log(result);
