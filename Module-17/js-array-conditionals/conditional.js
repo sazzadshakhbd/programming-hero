3200f1cf858b47659c36fae9bf747e87d1608051
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-01 08:23:46 +0600

    how to use basic if else condition

---
+++ Module-17/js-array-conditionals/conditional.js
@@ -0,0 +1,14 @@
+var iphonePrice = 79000;
+var myBudget = 9500;
+
+if (iphonePrice > myBudget) {
+    console.log('bye phone');
+}
+var chickenPrice = 500;
+var myMoney = 50;
+
+if (chickenPrice < myMoney) {
+    console.log('eat chicken')
+} else {
+    console.log('eat potato');
+}
