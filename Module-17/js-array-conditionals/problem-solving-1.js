86b36cde392c4d51f91ac523c27f4816f2caab50
NC-133 <nc-133@NC-133s-iMac.local>
2022-08-01 10:18:27 +0600

    problem-solving-1 done

---
+++ Module-17/js-array-conditionals/problem-solving-1.js
@@ -0,0 +1,10 @@
+var fruits = ['Apple', 'Banana', 'Orange']
+// console.log(fruits.indexOf('Banana'));
+fruits[1] = 'Mango';
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push('Watermelon');
+
+console.log(fruits);
