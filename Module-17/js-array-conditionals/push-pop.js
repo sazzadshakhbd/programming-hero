8601f805bf299a0488d6bdca405e4b787018ec44
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-01 08:03:41 +0600

    how to use push,pop, shift & unshift for array

---
+++ Module-17/js-array-conditionals/push-pop.js
@@ -0,0 +1,27 @@
+var numbers = [10, 20, 30, 40, 50, 60, 70]
+
+//  Use push to add element to an array as the last element by array
+numbers.push(80)
+console.log(numbers);
+
+var fruits = ['apple', 'orange', 'malta'];
+fruits.push('lichi');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+numbers.pop();
+console.log(numbers);
+
+fruits.unshift('mango', 'banana');
+console.log(fruits);
+
+numbers.unshift(1, 2, 3, 4, 5, 6, 7, 8, 9);
+console.log(numbers);
+
+fruits.shift();
+console.log(fruits);
+
+numbers.shift();
+console.log(numbers);
+
