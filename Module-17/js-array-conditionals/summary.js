72ee63f3b78b1173fe7b1cf968bb4e0bf18e9335
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-01 08:49:52 +0600

    module summary

---
+++ Module-17/js-array-conditionals/summary.js
@@ -0,0 +1,17 @@
+var destinition = ['sazek', 'coxsbazar', 'nepal', 'bali'];
+// var x = destinition.indexOf('bali');
+// console.log(destinition[2]);
+// console.log(destinition.length);
+// console.log(x);
+
+destinition.push('rajbari');
+console.log(destinition);
+
+destinition.pop();
+console.log(destinition);
+
+// conditionals
+
+// <, >, ==, !=, <=, >=, &&, ||
+
+// Array length
