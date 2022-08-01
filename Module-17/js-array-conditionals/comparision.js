285acc7b9fbd302b941ca6496df2c0436606c03b
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-01 08:17:04 +0600

    intro to comparison operator

---
+++ Module-17/js-array-conditionals/comparision.js
@@ -0,0 +1,28 @@
+console.log(5 > 6);
+console.log(5 < 7);
+console.log(5 == 7);
+console.log(6 == 6);
+console.log(5 != 8);
+console.log(5 != 5);
+// Less than or equal
+console.log(5 <= 8);
+console.log(8 <= 5);
+console.log(5 <= 5);
+// Greter than or equal
+console.log(5 >= 8);
+console.log(8 >= 5);
+console.log(8 >= 8);
+
+var num1 = 19;
+var num2 = 28;
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+
+// Multiple Condition
+money1 > money2 && result1 > result2 && height1 > height2
+
+money1 > money2 || result1 > result2 || height1 > height2
