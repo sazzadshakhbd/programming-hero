a0337f9ebeab67f53332e51aaea09214366f7c75
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-01 08:42:07 +0600

    how to define if else, else if & if unded if condition

---
+++ Module-17/js-array-conditionals/multistage-condition.js
@@ -0,0 +1,14 @@
+var money = 100;
+var danish = 45;
+var bread = 35;
+var biscuits = 20;
+
+if (danish >= money) {
+    console.log('danis khamu');
+} else if (bread < money) {
+    console.log('bread khamu');
+} else if (biscuits > money) {
+    console.log('biscuits khamu')
+} else {
+    console.log('taka nai')
+}
---
+++ Module-17/js-array-conditionals/nested-condition.js
@@ -0,0 +1,18 @@
+var math = true;
+var geomatry = true;
+var straightLine = false;
+
+if (math == true) {
+    if (geomatry == true) {
+        if (straightLine == true) {
+
+        } else {
+            console.log('baka pohe cholab na')
+        }
+    } else {
+        console.log('pithagoras hobe na')
+    }
+
+} else {
+    console.log('no-subject')
+}
