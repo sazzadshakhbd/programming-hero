d39bff4b2585fcb1f38bebf0c07f4f428a5db579
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-01 06:24:58 +0600

    Revision previos module

---
+++ Module-17/js-array-conditionals/variable.js
@@ -0,0 +1,25 @@
+var sunglassPrice = 120; // Number Type Variable
+var specialOneName = 'Arina Granda'; // String Type Variable
+var isSingle = true; // Bollean type variable
+/* 
+    Two types of number: 
+    a) Integer: 120
+    b) Float, Decimal: 3.65
+
+*/
+/*
+    variable nameimg convention: Rules & Best Practice
+    a) Single Word
+    b) No gap or space
+    c) No quote
+    d) No keyword
+    e) Can not start with  a number
+    f) Can not contain dash
+    g) Should use camel casr
+    h) Mathe Operations: +, _, *, /
+    i) Shorthand : +=, _=, ++, __
+    j) Convert: parseInt, parseFloat
+    k) ToFixed
+    l) Reminder: %
+
+*/
