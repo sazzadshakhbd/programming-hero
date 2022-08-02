fca1b30b8489349497752fcfcf0afc9b7b12b124
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-02 09:13:30 +0600

    how to decleare 10-1 using loop

---
+++ Module-18/loops-practice-problem/reverse.js
@@ -0,0 +1,19 @@
+var num = 1;
+while (num <= 10) {
+    // console.log(num);
+    num++;
+}
+
+var numbers = 10;
+while (numbers >= 1) {
+    // console.log(numbers);
+    numbers--;
+}
+
+for (var i = 1; i <= 10; i++) {
+    // console.log(i);
+}
+
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
