6d3457a8e742a5319cdc2d1badf47fff94eb8c99
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-02 07:28:06 +0600

    how to declear array loop using length

---
+++ Module-18/loops-practice-problem/array-loop.js
@@ -0,0 +1,13 @@
+// Display every element of an array
+var numbers = [45, 87, 89, 56, 32, 51, 25, 39, 99];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+var tableItems = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+for (var i = 0; i < tableItems.length; i++) {
+    var tableItem = tableItems[i];
+    console.log(tableItem);
+}
