e75d187bdef7bd1e33526fe6c781db6cd316604f
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-02 07:12:39 +0600

    how to declear for loop

---
+++ Module-18/loops-practice-problem/for.js
@@ -0,0 +1,22 @@
+var roastGiven = 0;
+while (roastGiven <= 7) {
+    // console.log('roast den');
+    roastGiven++;
+}
+
+for (var roastGiven = 0; roastGiven <= 7; roastGiven++) {
+    // console.log('roast den')
+}
+
+// simple version for loop
+for (i = 0; i <= 7; i++) {
+    // console.log(i);
+}
+
+for (var i = 0; i <= 20; i += 2) {
+    // console.log(i);
+}
+
+for (var i = 1; i <= 20; i += 2) {
+    console.log(i)
+}
