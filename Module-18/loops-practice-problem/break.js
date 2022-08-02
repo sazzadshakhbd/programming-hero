8f8be7ff4af24e916da60e8fb5a49a7b7972a201
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-02 07:44:40 +0600

    how to implement break and continue

---
+++ Module-18/loops-practice-problem/break.js
@@ -0,0 +1,34 @@
+// for (var i = 1; i < 20; i++) {
+//     if (i > 10) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// var roastGiven = 0;
+// while (roastGiven < 10) {
+//     roastGiven++;
+//     if (roastGiven > 3) {
+//         break;
+//     }
+//     console.log(roastGiven);
+// }
+
+// var tableItems = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+// for (var i = 0; i < tableItems.length; i++) {
+//     var tableItem = tableItems[i];
+//     if (tableItem == 'pen') {
+//         break
+//     }
+//     console.log(tableItem);
+// }
+
+var numbers = [45, 87, 89, 56, 32, 51, 25, 39, 99, 105];
+
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    if (number > 50) {
+        continue;
+    }
+    console.log(number);
+}
