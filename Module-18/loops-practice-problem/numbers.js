13f20767f6bfc077024ee6f74da66a2e8774686a
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-02 07:00:38 +0600

    how to findout add or even number using loop

---
+++ Module-18/loops-practice-problem/numbers.js
@@ -0,0 +1,31 @@
+/* 
+    Rules of loop declearation
+    a) Loop variable
+    b) Condition inside while
+    c) Loop body
+    d) Change the loop variable
+*/
+
+// Display 1-20
+var number = 1;
+while (number <= 20) {
+    // console.log(number);
+    number = number + 1;
+}
+
+// Display even number (1-20)
+
+var evenNumbers = 0;
+while (evenNumbers <= 20) {
+    // console.log(evenNumbers);
+    evenNumbers = evenNumbers + 2
+}
+
+// display odd number (1-20)
+
+var oddNumber = 1;
+while (oddNumber <= 20) {
+    console.log(oddNumber);
+    oddNumber = oddNumber + 2;
+
+}
