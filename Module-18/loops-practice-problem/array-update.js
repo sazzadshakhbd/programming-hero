5cb01ed40654b2bfd407d06ad29f4c4ca9bee822
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-02 09:32:09 +0600

    problem solved-1 for module-17

---
+++ Module-18/loops-practice-problem/array-update.js
@@ -0,0 +1,12 @@
+var fruits = ['apple', 'banana', 'orange'];
+var bananaIndex = fruits.indexOf('banana');
+console.log(bananaIndex);
+
+fruits[bananaIndex] = 'mango';
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push('watermelon');
+console.log(fruits);
