1b0755d47fb6f864ff3bf86775648e1f9faf5ba3
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-07 11:16:52 +0600

    how to decleare object

---
+++ Module-19/js-function-object/06-object.js
@@ -0,0 +1,27 @@
+var student = {
+    id: 15,
+    name: 'sazzad',
+    class: 8,
+    marks: 40
+}
+
+var mobile = {
+    brand: 'samsung',
+    price: 19000,
+    storage: "64gb",
+    camera: '7mp'
+}
+
+var myComputer = {
+    brand: 'lenevo',
+    price: 97000,
+    color: 'Black',
+    processor: 'i7'
+}
+// console.log(myComputer);
+// console.log(myComputer.brand);
+// console.log(myComputer.price);
+
+// How to set and get object valuse
+myComputer.processor = 'i3';
+console.log(myComputer);
