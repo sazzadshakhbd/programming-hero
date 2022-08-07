56137447dd3760d4d49ba2e3f548b3370d67ed5b
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-07 19:13:01 +0600

    how to define switch

---
+++ Module-19/js-function-object/09-switch.js
@@ -0,0 +1,37 @@
+const color = 'yellow';
+
+if (color === 'green') {
+    console.log('green');
+} else if (color === 'blue') {
+    console.log('blue');
+} else if (color === 'red') {
+    console.log('red');
+} else if (color === 'white') {
+    console.log('white');
+} else if (color === 'yellow') {
+    console.log('yellow')
+} else {
+    console.log('black')
+}
+
+// Switch Example
+
+switch (color) {
+    case ('green'):
+        console.log(green);
+        break;
+    case ('blue'):
+        console.log('blue');
+        break;
+    case ('white'):
+        console.log('white');
+        break;
+    case ('red'):
+        console.log('red');
+        break;
+    case ('yellow'):
+        console.log('yellow');
+        break;
+    default:
+        console.log('Black')
+}
