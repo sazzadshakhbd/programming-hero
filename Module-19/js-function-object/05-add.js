a3e2bdbb9ced5bc530824f7e2eeca74fcbe43801
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-07 11:07:33 +0600

    how to add a return function another way

--- Module-19/js-function-object/05-add.js
+++ Module-19/js-function-object/05-add.js
@@ -10,3 +10,12 @@
     // Basic structur
 
 */
+function add(num1, num2) {
+    var total = num1 + num2;
+    return total;
+}
+const result1 = add(10, 15);
+const result2 = add(20, 30);
+const totalMarks = add(result1, result2);
+console.log(totalMarks);
+
