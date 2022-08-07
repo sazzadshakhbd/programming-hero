74f42f6c5123d015b7687a5b40239ef86743bfe5
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-07 10:08:02 +0600

    how to add parameter differnet differnt way

--- Module-19/js-function-object/03-parameter.js
+++ Module-19/js-function-object/03-parameter.js
@@ -1,5 +1,34 @@
 function bringSingara(money) {
-    console.log('mama singara den')
-    console.log(money);
+    // console.log('mama singara den')
+    // console.log(money);
 }
 bringSingara(250);
+
+
+function bringSingara(money) {
+    // console.log('Taka disen: ', money);
+    // console.log('ei nen singara');
+}
+bringSingara(100);
+
+
+function penPrice(money) {
+    // console.log('Price: ', money)
+}
+var penPriceUpdate = 20;
+penPrice(penPriceUpdate);
+
+
+function add(num1, num2) {
+    // console.log('Going to add ', num1, num2)
+}
+// add();
+// add(20);
+add(125, 96);
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum)
+}
+sum(15, 98, 56);
