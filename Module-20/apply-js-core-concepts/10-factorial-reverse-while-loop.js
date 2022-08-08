41ebfd49c3d3c75763d1b460d850a9aef7bcf734
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-08 10:25:03 +0600

    add factorial on reverse way using whil loop

---
+++ Module-20/apply-js-core-concepts/10-factorial-reverse-while-loop.js
@@ -0,0 +1,11 @@
+function factorial(number) {
+    let i = number;
+    let result = 1;
+    while (i >= 1) {
+        result = result * i;
+        i--;
+    }
+    return result;
+}
+var totalNumber = factorial(6);
+console.log(totalNumber);
