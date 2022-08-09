8d9c71e266b0850a5828fb74f1d7be5cbb4dcb93
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-09 21:36:09 +0600

    how to convert feet to inch

---
+++ Module-20-5/recap.js
@@ -0,0 +1,39 @@
+/*
+    // Six core concept must be know
+
+    1) Variable: 
+        decleare, naming convension, variable types, primitive, math operations
+    2) Conditionals:
+        comparision, >, <, <=, >=, ==, ===, !=, !==, if-else, else-if
+    3) Array:
+        decleare, index, get values, set values, indexof, push, pop
+    4) Loop
+        for-loop, while-loop, break, continue
+    5) Function
+        decleare, parameter, return, call, use the return value from a function
+    6) Object
+        decleare, properties, keys, values, get property value, set value, loop object
+
+*/
+
+/*
+    // Google Search
+
+    1. function declearing vs function expression
+    2. Arrow Function
+    3. Loop Function
+    4. Array vs Object
+    5. Integer vs Float
+    6. == vs ===
+    7. Difference between let, const, var
+
+*/
+
+/*
+    // Parctice Task
+    
+    1. celius ---> Farenhite
+    2. Farenhite ---> celcius
+    3. Simple Interst
+    4. Complex Interset
+*/
---
+++ Module-20-6-Conceptual-Session/09-Problem-Convert-Feet-Inch.js
@@ -0,0 +1,13 @@
+/*
+    Problem-2:
+
+    Write a function that will take feet as the input parameter and will convert it into inch and will return the result in inch.
+*/
+
+function feetToInch(feet) {
+    const inchValue = feet * 12;
+    return inchValue;
+}
+var result = feetToInch(5);
+console.log(result);
+
