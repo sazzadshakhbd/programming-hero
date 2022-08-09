2fe9acbf27ec462e061c75c4cc0be602ad7dc53c
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-09 20:00:42 +0600

    how to decleare, calling & add numbers using function

---
+++ Module-20-6-Conceptual-Session/01-Function.js
@@ -0,0 +1,19 @@
+// How to decleare a function?
+function welcomeMessage(name) {
+    console.log('Hello', name)
+}
+welcomeMessage('sazzad');
+
+// How to calling a function?
+
+welcomeMessage('sobuz');
+
+// How to add two numbers using function?
+
+function addNumber(a, b) {
+    const result = a + b;
+    return result;
+}
+var totalResult = addNumber(2, 5);
+var totalResult = addNumber(2, 19);
+console.log(totalResult);
