63feec9334c31199150cd77238cb1641f984cd07
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-10 07:03:12 +0600

    problem-find prime number

--- Module-20-6-Conceptual-Session/17-Problem-Prime-Number.js
+++ Module-20-6-Conceptual-Session/17-Problem-Prime-Number.js
@@ -1,23 +1,29 @@
 /*
     Problem-9:
 
-   Write a function that will take a number and will check the number is positive
-or negative.
+Write a JavaScript function that accepts a number as a parameter and check
+the number is prime or not.
 
 
 */
 
-function checkPositiveNegative(num) {
-    if (num == 0) {
-        return (num, 'Is zero');
-    } else if (num > 0) {
-        return (num, 'Is Posative');
+function primeNumber(num) {
+    let count = 0;
+    for (let i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            count++
+        }
+    }
+    if (count == 2) {
+        console.log(num, 'Prime Number')
     } else {
-        return (num, 'is Negative');
+        console.log(num, 'Just Number')
     }
 }
-var result = checkPositiveNegative(0);
-console.log(result);
 
 
-
+console.log(primeNumber(2));
+console.log(primeNumber(1));
+console.log(primeNumber(0));
+console.log(primeNumber(6));
+console.log(primeNumber(7));
