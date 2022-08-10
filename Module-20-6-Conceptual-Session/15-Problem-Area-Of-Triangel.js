b5a75789064e92dd22070de0a4eb5b94fe593b5f
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-10 06:42:27 +0600

    problem-area of triangle

---
+++ Module-20-6-Conceptual-Session/15-Problem-Area-Of-Triangel.js
@@ -0,0 +1,14 @@
+/*
+    Problem-7:
+
+     Write a function named findArea() that will take base and height of a triangle and will return the area of triangle.
+
+*/
+
+function findArea(base, height) {
+    const area = 0.5 * base * height;
+    return area;
+}
+var result = findArea(5, 7);
+console.log(result);
+
