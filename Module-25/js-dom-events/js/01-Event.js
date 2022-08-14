a7449c05a7ee05f5129e14047b74726a0a5d7d11
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-14 20:35:00 +0600

    how to decleare addEventListener 3 ways

--- Module-25/js-dom-events/js/01-Event.js
+++ Module-25/js-dom-events/js/01-Event.js
@@ -17,3 +17,23 @@ const makePurpleButton = document.getElementById('makePurple');
 makePurpleButton.onclick = function makePurpleFunction() {
     document.body.style.backgroundColor = 'purple';
 }
+
+// How to decleare a onclick event using addEventListener [Option-1]
+const makePinkButton = document.getElementById('makePink');
+makePinkButton.addEventListener('click', makePinkFunction);
+
+function makePinkFunction() {
+    document.body.style.backgroundColor = 'pink'
+}
+
+// How to decleare a onclick event using addEventListener [Option-2]
+const makeGreenButton = document.getElementById('makeGreen');
+makeGreenButton.addEventListener('click', function makeGreenFunction() {
+    document.body.style.backgroundColor = 'green';
+});
+
+// How to decleare a onclick event using addEventListener [Final]
+
+document.getElementById('makeTeal').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'teal'
+})
