c80f047a62bd2b93aaa0428f3cd28a441d31c779
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-17 21:48:37 +0600

    how to add & remove class using javascript

---
+++ Module-25-6-Conceptual-Session/add-remove.js
@@ -0,0 +1,8 @@
+const addClassMethod = document.querySelector('h2');
+addClassMethod.classList.add('third');
+
+const anotherClassAdd = document.getElementById('heading');
+anotherClassAdd.classList.add('third')
+
+const removeClassMethod = document.querySelector('h2');
+removeClassMethod.classList.remove('third');
--- Module-25-6-Conceptual-Session/style.js
+++ Module-25-6-Conceptual-Session/style.js
@@ -1,4 +1,4 @@
 const styleElement = document.getElementById('heading');
-styleElement.style.color = '#fff';
-styleElement.style.backgroundColor = 'teal';
-styleElement.style.padding = '1rem';
+// styleElement.style.color = '#fff';
+// styleElement.style.backgroundColor = 'teal';
+// styleElement.style.padding = '1rem';
