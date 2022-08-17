4f918159081bbbdf7d1c7abc775c6e9b5a5525e3
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-17 22:30:59 +0600

    how to decleare get and set attribute

---
+++ Module-25-6-Conceptual-Session/get-set-attribute.js
@@ -0,0 +1,11 @@
+const getid = document.getElementById('heading');
+console.log(getid.getAttribute('id'));
+
+const x = document.getElementById('main-container');
+console.log(x.getAttribute('id'));
+
+const y = document.getElementById('heading');
+const z = y.setAttribute('class', 'test')
+
+const a = document.getElementById('main-container');
+const b = a.setAttribute('class', 'hide')
