6510e0c57ee709d8a5bc6e83db346030f2d05714
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-18 07:21:13 +0600

    how to use click handeler and click listener

---
+++ Module-25-6-Conceptual-Session/event/event.js
@@ -0,0 +1,9 @@
+// Event Handelar
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// Event Listener
+document.getElementById('btn-green').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
+})
