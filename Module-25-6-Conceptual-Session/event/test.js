04ba4372c5276d4ebd42dc8d5212c6d17a423d6b
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-20 10:44:47 +0600

    how to use template string

---
+++ Module-25-6-Conceptual-Session/event/test.js
@@ -0,0 +1,5 @@
+const childBoys = 'Adib';
+const childGirls = 'Adiba';
+
+console.log(`My Son Name is ${childBoys} and My daughter Name is ${childGirls}`);
+console.log('I have one son name ' + childBoys + ' and I have one daughter name ' + childGirls);
