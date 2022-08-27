88cb59becb346e8314416477c80176e22fe9814f
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-27 19:28:37 +0600

    add intro text for module 31.5, 31

---
+++ Milestone-6-Intermediate-JavaScript-API/Module-31-5-Conceptual-Session/01-letconst.js
@@ -0,0 +1,19 @@
+// var name = 'sazzad'; 
+// names = 'sobuz'; // Reassing Possible
+
+// var names = 'mofiz'; // Redifine Possible
+// console.log(names);
+
+
+// let name = 'sazzad';
+// names = 'sobuz'; // Reassing Possible
+
+// let names = 'mofiz'; // Redifine not Possible
+// console.log(names);
+
+
+// const name = 'sazzad'; 
+// names = 'sobuz'; // Reassing not Possible
+
+// const names = 'mofiz'; // Redifine not Possible
+// console.log(names);
---
+++ Milestone-6-Intermediate-JavaScript-API/Module-31-5-Conceptual-Session/02-scope.js
@@ -0,0 +1,10 @@
+// Global Scope
+// Local Scope
+// Block Scope
+
+const x = 5;
+{
+    const y = 10;
+    console.log('Inside Block: ', y);
+}
+// console.log('outside block: ', y)
