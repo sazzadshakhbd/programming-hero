4e3ede27f424460e7e7ef0282eadd17ebc9ee446
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-28 11:26:26 +0600

    how to add multiple object in json array

---
+++ Milestone-6-Intermediate-JavaScript-API/Module-33 API Examples and edge cases/API-Examples/js/02-Random-User.js
@@ -0,0 +1,33 @@
+const url = 'https://randomuser.me/api/?results=10';
+function loadData() {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayData(data.results))
+}
+displayData = usersInfo => {
+    // console.log(users)
+    const mainContainer = document.getElementById('user-container');
+    // console.log(mainContainer);
+    for (users of usersInfo) {
+        console.log(users);
+        const div = document.createElement('div');
+        div.classList.add('contianer');
+        div.innerHTML = `
+            <ul>
+                <li>Cell Number: ${users.cell}</li>
+                <li>Age: ${users.dob.age}</li>
+                <li>Date: ${users.dob.date}</li>
+                <li>Email: ${users.email}</li>
+                <li>Gender: ${users.gender}</li>
+                <li>ID-Name: ${users.id.name}</li>
+                <li>ID-Value: ${users.id.value}</li>
+                <li>City: ${users.location.city}</li>
+                <li>Latitude: ${users.location.coordinates.latitude}</li>
+                <li>Longitude: ${users.location.coordinates.longitude}</li>
+                
+            </ul>
+        `
+        mainContainer.appendChild(div);
+    }
+}
+loadData();
