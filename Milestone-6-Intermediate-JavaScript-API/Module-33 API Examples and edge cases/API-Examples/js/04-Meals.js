8c2051cbd31428d08b247b50ad40efe826bc2180
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-29 10:23:30 +0600

    how to add onclick see details meals dynamically

--- Milestone-6-Intermediate-JavaScript-API/Module-33 API Examples and edge cases/API-Examples/js/04-Meals.js
+++ Milestone-6-Intermediate-JavaScript-API/Module-33 API Examples and edge cases/API-Examples/js/04-Meals.js
@@ -10,11 +10,11 @@ const displayMeals = (meals) => {
     mealsContainer.innerHTML = ``;
     // console.log(mealsContainer);
     meals.forEach(meal => {
-        console.log(meal)
+        // console.log(meal)
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-        <div class="card">
+        <div class="card" onclick="loadMealDetails(${meal.idMeal})">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -33,4 +33,29 @@ const searchFood = () => {
     loadMeals(searchText);
     searchField.value = '';
 }
-// loadMeals('pizza');
+const loadMealDetails = (idMeal) => {
+    // console.log(idMeal);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
+
+const displayMealDetails = (meal) => {
+    // console.log(meal)
+    const detailsContainer = document.getElementById('details-container');
+    detailsContainer.innerHTML = ``;
+    const createDiv = document.createElement('div');
+    createDiv.classList.add('card');
+    createDiv.classList.add('mb-5')
+    createDiv.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 170)}</p>
+    </div>
+    `;
+    detailsContainer.appendChild(createDiv);
+}
+loadMeals('');
