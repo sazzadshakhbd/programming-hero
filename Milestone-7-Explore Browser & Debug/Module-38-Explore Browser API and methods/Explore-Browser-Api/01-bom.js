9760a7513878fa6009bf0511e4fe48f2cb31a97f
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-09-06 08:01:26 +0600

    previous all work this commited

---
+++ Milestone-7-Explore Browser & Debug/Module-38-Explore Browser API and methods/Explore-Browser-Api/01-bom.js
@@ -0,0 +1,3 @@
+// What is Bom in browser in javascript? 'google search'
+// Dom vs Bom ?
+// waht is Javascript browser api?
---
+++ Milestone-7-Explore Browser & Debug/Module-38-Explore Browser API and methods/Explore-Browser-Api/02-Prompt.js
@@ -0,0 +1,28 @@
+// alert("hello!");
+
+const showAlert = () => {
+    const num = Math.random() * 10;
+    console.log(num);
+    if (num > 5) {
+        // alert('Number is lower 5')
+    }
+    // alert('Ki khobr?')
+}
+
+const askSomething = () => {
+    const dicision = confirm('Are you coming picnic?');
+    // console.log(dicision);
+    if (dicision === true) {
+        alert('dosot 500 taka bkash kor')
+    } else {
+        console.log('DGM - Dure giye misti kha');
+    }
+}
+
+const getUserInfo = () => {
+    const name = prompt('Tell us your name');
+    // console.log(name);
+    if (!!name) {
+        console.log('Welcome here', name)
+    }
+}
