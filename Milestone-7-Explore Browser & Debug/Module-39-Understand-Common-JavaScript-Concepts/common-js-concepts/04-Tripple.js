30eec545a7ac34db7683fccccd8128cde47433be
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-09-12 08:16:42 +0600

    details of triple equal

---
+++ Milestone-7-Explore Browser & Debug/Module-39-Understand-Common-JavaScript-Concepts/common-js-concepts/04-Tripple.js
@@ -0,0 +1,50 @@
+const first = 2;
+const second = 2;
+
+if (first == second) {
+    console.log('Values are equal')
+} else {
+    console.log('Values are not equal')
+}
+
+const a = 2;
+const b = '2';
+
+// if (a == b) {
+if (a === b) {
+    console.log('Values are equal')
+} else {
+    console.log('Values are not equal')
+}
+
+
+// const c = 1;
+// const c = 2;
+const c = 0;
+// const d = true;
+const d = false;
+
+if (a == b) {
+    // if (a === b) {
+    console.log('Values are equal')
+} else {
+    console.log('Values are not equal')
+}
+
+// Type Coercion or Type Conversion or Type Casting
+
+
+// const e = {};
+// const f = {};
+
+// const e = { x: 5 }
+// const f = { x: 5 }
+const e = []
+const f = []
+
+if (e == f) {
+    // if (e === f) {
+    console.log('Values are equal')
+} else {
+    console.log('Values are not equal')
+}
