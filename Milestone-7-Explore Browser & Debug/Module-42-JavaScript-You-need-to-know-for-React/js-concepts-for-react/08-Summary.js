14ffdd286134de0c0654854c4ec29e4729a438f1
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-09-14 23:01:26 +0600

    must know this topics

---
+++ Milestone-7-Explore Browser & Debug/Module-42-JavaScript-You-need-to-know-for-React/js-concepts-for-react/08-Summary.js
@@ -0,0 +1,66 @@
+/*
+    // Must Learn This Topics
+
+    A) Fundamental Topics
+    ---------------------
+        1.1 Variable: (Let, Const) and 3 types of variable
+        1.2 Condition: (<, >, ===, !==, <=, >=) &&,  ||, if, else-if, else
+        1.3 Array: Declearation, Length, Index, Push, Pop, IndexOf, Includes
+        1.4 Loop: For Loop, While Loop
+        1.5 Function: Return, Parameter
+        1.6 Object: Property, Including Array, Including Object.
+
+    B) ES6 Topics
+    -----------------
+        1.1 Template String ${}.
+        1.2 Spread Operator(...)
+            a) Copy an array then add a new element to an array
+            b) Use Filter to remove an element from an array.
+        1.3 Arrow Function
+            a) No Parameter ()
+            b) Single Parameter
+            c) Multiple Patameter
+            d) MultiLine Parameter
+        1.4 Destructuring
+            a) Object Destructuring
+            b) Array Destructuring 
+        1.5 Object Declearation Shorthand
+        1.6 Function Parameter Default Value
+        1.7 Optional Chaining (?)
+
+    C) Browser API
+    -----------------
+        1.1 Local Storage, Session Storage
+        1.2 Locaion API
+        1.3 History API
+        1.4 Fetch
+    
+    D) Others
+    ------------
+        1.1 Array: Filter, Map, ForEach, Find
+        1.2 Ternary Operator
+        1.3 Logical & Logical or && ||
+        1.4 JSON: Strinigfy, Parse
+        1.5 +,
+
+*/
+
+
+// Example: Object Declearation Shorthand
+const x = 50;
+const y = 60;
+const obj = {
+    x: x,
+    y: y
+}
+
+const obj2 = { x, y };
+
+const name = 'sazzad';
+const area = 'dhaka';
+
+const nobab = {
+    name: name,
+    area: area
+}
+const nobab2 = { name, area }
