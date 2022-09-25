f4deb2a38d1554b3b34e5f6f0f799776c991e125
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-09-25 17:55:43 +0600

    product-info design done

--- Milestone-8-Simple-React/Module-48-Simple-React-SPA-with-simple-e-commerce/ema-john-simple/src/components/Product/Product.js
+++ Milestone-8-Simple-React/Module-48-Simple-React-SPA-with-simple-e-commerce/ema-john-simple/src/components/Product/Product.js
@@ -10,6 +10,15 @@ const Product = (props) => {
                 <div className='text-center'>
                     <img src={img} alt="Shoe" />
                 </div>
+                <div className="product-info">
+                    <h2>{name}</h2>
+                    <h3>Price: ${price}</h3>
+                    <p>Manufacturer: {seller}</p>
+                    <p>Rating: {ratings}</p>
+                </div>
+                <div className="cart-btn">
+                    <button>Add to Cart</button>
+                </div>
             </div>
         </div>
     );
