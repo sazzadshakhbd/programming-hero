d10cdbe87550f24abf4131f15a914a5ad5c45909
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-09-26 10:03:41 +0600

    done ema-john project

--- Milestone-8-Simple-React/Module-49-(advanced)-Cart-calculation-Save-and-Retrieve-cart/ema-john-simple/src/components/Shop/Shop.js
+++ Milestone-8-Simple-React/Module-49-(advanced)-Cart-calculation-Save-and-Retrieve-cart/ema-john-simple/src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb, getStoreCard } from '../../utilities/fakedb';
 import './Shop.css';
 
 const Shop = () => {
@@ -8,14 +9,44 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
+        // console.log('product loads before')
         fetch('products.json')
             .then(res => res.json()
                 .then(data => setProducts(data)))
     }, []);
 
-    const handelAddToCart = (product) => {
-        const newCart = [...cart, product];
+    useEffect(() => {
+        // console.log('local storage first line')
+        const storedCart = getStoreCard();
+        const saveCart = []
+        for (const id in storedCart) {
+            // console.log(id)
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct)
+            }
+            console.log(addedProduct)
+        }
+        // console.log(storedCart)
+        setCart(saveCart)
+    }, [products])
+
+    const handelAddToCart = (selectedProduct) => {
+        let newCart = []
+        const exists = cart.find(product => product.id === selectedProduct.id)
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct]
+        } else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists]
+        }
+        // const newCart = [...cart, selectedProduct];
         setCart(newCart);
+        addToDb(selectedProduct.id)
         // console.log(product);
     }
 
