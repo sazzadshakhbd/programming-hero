068be68227127ea376e8224b5e731146db029128
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-09-27 09:27:11 +0600

    ES6 Modules, import default, relative path

---
+++ Milestone-8-Simple-React/Module-47-5-Modules-and-data-storage-Integration/react-storage-and-handaler/src/components/Shoes/Shoes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { add, multiply } from '../Utilities/Calculate';
+// import add from '../Utilities/Calculate';
+
+const Shoes = () => {
+    const first = 30;
+    const second = 20;
+    const total = multiply(first, second);
+    const result = add(first, second)
+
+    return (
+        <div>
+            <h1>This is shoes component</h1>
+            <h2>Total: multiply-{total} and add-{result}</h2>
+        </div>
+    );
+};
+
+export default Shoes;
