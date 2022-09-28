83546c55e4f5865a5d4bc9be2d4593c861fcfbba
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-09-29 01:44:10 +0600

    simple intro to array reduce

--- Milestone-8-Simple-React/Module-47-5-Modules-and-data-storage-Integration/react-storage-and-handaler/src/components/Cosmetics/Cosmetics.js
+++ Milestone-8-Simple-React/Module-47-5-Modules-and-data-storage-Integration/react-storage-and-handaler/src/components/Cosmetics/Cosmetics.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Ornaments from '../Ornaments/Ornaments';
+import { getTotal } from '../Utilities/Calculate';
 
 const Cosmetics = () => {
     const [cosmetics, setCosmetics] = useState([])
@@ -8,9 +9,12 @@ const Cosmetics = () => {
             .then(res => res.json())
             .then(data => setCosmetics(data))
     }, [])
+
+    const total = getTotal(cosmetics)
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
+            <p>Money needed : {total}</p>
             {
                 cosmetics.map(cosmetic => <Ornaments id={cosmetic.id} key={cosmetic.id} name={cosmetic.name} price={cosmetic.price}></Ornaments>)
             }
--- Milestone-8-Simple-React/Module-47-5-Modules-and-data-storage-Integration/react-storage-and-handaler/src/components/Utilities/Calculate.js
+++ Milestone-8-Simple-React/Module-47-5-Modules-and-data-storage-Integration/react-storage-and-handaler/src/components/Utilities/Calculate.js
@@ -4,5 +4,27 @@ const add = (first, second) => {
 const multiply = (first, second) => {
     return first * second
 }
+
+// const numbers = [20, 24, 237, 65, 89];
+// const sumReducer = (previous, current) => previous + current;
+// const total = numbers.reduce(sumReducer, 0)
+
+// const items = [
+//     { id: 1, name: 'Tiger', price: 500 },
+//     { id: 2, name: 'Cat', price: 400 },
+//     { id: 3, name: 'Dog', price: 300 },
+//     { id: 4, name: 'Cow', price: 200 },
+//     { id: 5, name: 'Goat', price: 100 },
+// ]
+// const itemReducer = (previous, current) => previous + current.price
+// const itemTotal = items.reduce(itemReducer, 0)
+
+
+const getTotalPrice = products => {
+    const reducer = (previous, current) => previous + current.price;
+    const total = products.reduce(reducer, 0);
+    return total;
+}
+
 // export default add
-export { add, multiply };
+export { add, multiply, getTotalPrice as getTotal };
