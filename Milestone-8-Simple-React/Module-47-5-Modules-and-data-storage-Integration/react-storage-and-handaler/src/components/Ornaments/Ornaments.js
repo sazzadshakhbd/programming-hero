ab8ed269201ed4f9ec1827bf7e9c825c041bb064
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-09-28 22:24:46 +0600

    How to Create Event handler with parameters

--- Milestone-8-Simple-React/Module-47-5-Modules-and-data-storage-Integration/react-storage-and-handaler/src/components/Ornaments/Ornaments.js
+++ Milestone-8-Simple-React/Module-47-5-Modules-and-data-storage-Integration/react-storage-and-handaler/src/components/Ornaments/Ornaments.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import './Ornaments.css';
 
 const Ornaments = (props) => {
-    const { name, price, id } = props
+    const { name, price, id } = props;
+    const addToCrt = (id) => {
+        console.log('item added', id);
+    }
+    const addToCartActive = () => {
+        addToCrt(id);
+    }
     return (
-        <div>
+        <div className='product'>
             <h2>Buy this products: {name}</h2>
             <p>Only for: ${price}</p>
             <p>Products id: {id}</p>
+            <button onClick={addToCartActive}>Add To Cart</button>
+            <button onClick={() => addToCrt(id)}>Purchase</button>
         </div>
     );
 };
