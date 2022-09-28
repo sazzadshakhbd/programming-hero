42f713686043e192ccf389e194f7d9bcafc77a61
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-09-28 23:57:52 +0600

    Store multiple data in an Object with local storage

--- Milestone-8-Simple-React/Module-47-5-Modules-and-data-storage-Integration/react-storage-and-handaler/src/components/Utilities/fakedb.js
+++ Milestone-8-Simple-React/Module-47-5-Modules-and-data-storage-Integration/react-storage-and-handaler/src/components/Utilities/fakedb.js
@@ -1,15 +1,27 @@
 // Use localstorage to manage cart data
 const addToDb = (id) => {
-    const quantity = localStorage.getItem(id);
-    if (quantity) {
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity)
-        console.log('Already exist');
+
+    // let shoppingCart = {};
+    let shoppingCart;
+
+    // Get the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart')
+    if (storedCart) {
+        // console.log(typeof storedCart)
+        shoppingCart = JSON.parse(storedCart)
     } else {
-        console.log('New Iem')
-        localStorage.setItem(id, 1)
+        shoppingCart = {}
     }
-    // localStorage.setItem(id, 1)
+
+    // Add Quantity
+    const quantity = shoppingCart[id];
+    if (quantity) {
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    } else {
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 }
 
 export { addToDb }
