58223d972c236e5885e559b9de85fb19a7f9d846
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-05 21:47:20 +0600

    Install React Bootstrap in your react application

--- Milestone-9-React-Router-and-States/Module-51-Bootstrap-Tailwind-CSS-Axios-Rechart/react-with-bootstrap/src/App.js
+++ Milestone-9-React-Router-and-States/Module-51-Bootstrap-Tailwind-CSS-Axios-Rechart/react-with-bootstrap/src/App.js
@@ -1,23 +1,14 @@
-import CardGroup from "./components/CardGroup/CardGroup";
+import Buttons from "./components/Buttons/Buttons";
+import Spinners from './components/Spinners/Spinners';
+import GridCard from './components/GridCard/GridCard';
 
 
 function App() {
   return (
     <div className="text-center m-5">
-      {/* <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <button type="button" className="btn btn-success">Success</button>
-      <button type="button" className="btn btn-danger">Danger</button>
-      <button type="button" className="btn btn-warning">Warning</button>
-      <button type="button" className="btn btn-info">Info</button>
-      <button type="button" className="btn btn-light">Light</button>
-      <button type="button" className="btn btn-dark">Dark</button>
-      <button type="button" className="btn btn-link">Link</button> */}
-
-      {/* <div className="spinner-border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </div> */}
-      <CardGroup></CardGroup>
+      <Buttons></Buttons>
+      <Spinners></Spinners>
+      <GridCard></GridCard>
     </div>
   );
 }
---
+++ Milestone-9-React-Router-and-States/Module-51-Bootstrap-Tailwind-CSS-Axios-Rechart/react-with-bootstrap/src/components/Spinners/Spinners.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const Spinners = () => {
+    return (
+        <div>
+            <Spinner animation="grow" variant="secondary" />
+        </div>
+    );
+};
+
+export default Spinners;
