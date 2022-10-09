5a29a78460441165f648cfe1870346f8c2117c07
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-09 19:40:07 +0600

    (optional) Browser Router, Routes and Route Component

---
+++ Milestone-9-React-Router-and-States/Module-52-Simple-React-Router-Example/explore-react-router-component/src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './components/About';
+import Header from './components/Header';
+import Home from './components/Home';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/products" element={<Products></Products>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
