015a67cd578d14885dfd9b7e2364c9f519910374
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-11 00:31:18 +0600

    Recap React Router setup and intro to order review page

--- Milestone-9-React-Router-and-States/Module-53-ema-john-with-router-and-custom-hook/ema-john-simple/src/layouts/Main.js
+++ Milestone-9-React-Router-and-States/Module-53-ema-john-with-router-and-custom-hook/ema-john-simple/src/layouts/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header/Header';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Shop></Shop>
+            <Outlet></Outlet>
         </div>
     );
 };
