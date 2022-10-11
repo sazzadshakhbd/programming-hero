fcb118d02633465ceb16ecf224d1e250b440804b
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-11 09:21:37 +0600

    Recap of React Router Setup

---
+++ Milestone-9-React-Router-and-States/Module-54-Router-deploy-Simple-Context-API-devtool/t-shirt-mania/src/_steps.js
@@ -0,0 +1,12 @@
+/*
+    Basic Router Setup
+    -------------------
+    1. npm i react-router-dom
+    2. Create a router using createBrowserRouter
+    3. Add routeProvider and set route prop
+    4. Create a lot of components
+    5. Create main layout with some common part and one changing part based on the route
+    6. Set the main layout at the root of the route
+    7. Set children routes
+    8. Set links on the header components
+*/
