6e7aa1f34d3a95f20544b42e0d22c5f31b71ea8b
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-12 07:25:49 +0600

    Recap Remove from cart and not allow duplicate

--- Milestone-9-React-Router-and-States/Module-54-Router-deploy-Simple-Context-API-devtool/t-shirt-mania/src/components/Cart/Cart.js
+++ Milestone-9-React-Router-and-States/Module-54-Router-deploy-Simple-Context-API-devtool/t-shirt-mania/src/components/Cart/Cart.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({ cart, handelRemoveItem }) => {
     return (
         <div>
             <h3>Order Summary</h3>
+            <h4>Order Quantity: {cart.length}</h4>
+            {
+                cart.map(tshirt =>
+                    <p key={tshirt._id}>
+                        {tshirt.name}
+                        <button onClick={() => handelRemoveItem(tshirt)}>X</button>
+                    </p>)
+            }
         </div>
     );
 };
--- Milestone-9-React-Router-and-States/Module-54-Router-deploy-Simple-Context-API-devtool/t-shirt-mania/src/components/Home/Home.js
+++ Milestone-9-React-Router-and-States/Module-54-Router-deploy-Simple-Context-API-devtool/t-shirt-mania/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import Tshirt from '../Tshirt/Tshirt';
@@ -6,15 +6,32 @@ import './Home.css';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([])
+    const handelToCart = (tshirt) => {
+        // console.log(tshirt)
+        const esists = cart.find(ts => ts._id === tshirt._id);
+        if (esists) {
+            alert('t-shirt already added')
+        } else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+            alert('Successfully Added')
+        }
+    }
+    const handelRemoveItem = (tshirt) => {
+        // console.log(tshirt);
+        const remaining = cart.filter(ts => ts._id !== tshirt._id)
+        setCart(remaining);
+    }
     return (
         <div className='home-container'>
             <div className="t-shirt-container">
                 {
-                    tshirts.map(tshirt => <Tshirt key={tshirt.id} tshirt={tshirt}></Tshirt>)
+                    tshirts.map(tshirt => <Tshirt handelToCart={handelToCart} key={tshirt._id} tshirt={tshirt}></Tshirt>)
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart cart={cart} handelRemoveItem={handelRemoveItem}></Cart>
             </div>
         </div>
     );
