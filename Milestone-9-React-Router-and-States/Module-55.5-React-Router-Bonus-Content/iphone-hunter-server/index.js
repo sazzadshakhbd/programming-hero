52382f23cd0018bf65b23a8b3aa056a4c40d1c70
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-17 08:48:41 +0600

    iphone hunter server add cors

--- Milestone-9-React-Router-and-States/Module-55.5-React-Router-Bonus-Content/iphone-hunter-server/index.js
+++ Milestone-9-React-Router-and-States/Module-55.5-React-Router-Bonus-Content/iphone-hunter-server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const phones = require('./phones.json')
 const port = 5000;
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Iphone Hunter Data nodemon')
