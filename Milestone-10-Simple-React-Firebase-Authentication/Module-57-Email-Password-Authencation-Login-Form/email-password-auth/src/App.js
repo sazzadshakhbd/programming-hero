540e8785f7bd2beb80bfb883438990ba418fa301
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-18 09:29:58 +0600

    Simple form, input, change, blur, submit, preventDefault

--- Milestone-10-Simple-React-Firebase-Authentication/Module-57-Email-Password-Authencation-Login-Form/email-password-auth/src/App.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-57-Email-Password-Authencation-Login-Form/email-password-auth/src/App.js
@@ -5,13 +5,25 @@ import app from './firebase/firebase.init';
 
 const auth = getAuth(app);
 
+const handlerOnSubmit = (event) => {
+  event.preventDefault();
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  console.log(email, password)
+}
+const onChangeEmailHandler = (event) => {
+  console.log(event.target.value)
+}
+const onChangePasswordHandler = (event) => {
+  console.log(event.target.value);
+}
 function App() {
   return (
     <div className="App">
-      <form>
-        <input type="email" name="" id="" placeholder='Your Email' />
+      <form onSubmit={handlerOnSubmit}>
+        <input onBlur={onChangeEmailHandler} type="email" name="email" id="" placeholder='Your Email' />
         <br></br>
-        <input type="password" name="" id="" placeholder='Your Password' />
+        <input onBlur={onChangePasswordHandler} type="password" name="password" id="" placeholder='Your Password' />
         <br></br>
         <button type="submit">Register</button>
       </form>
--- Milestone-10-Simple-React-Firebase-Authentication/Module-57-Email-Password-Authencation-Login-Form/email-password-auth/src/_step.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-57-Email-Password-Authencation-Login-Form/email-password-auth/src/_step.js
@@ -1,9 +1,14 @@
 /*
     Auth Terminology
     ------------------
-
     "Register" or "Sign Up" [First time create user]
     "log in" or "sign in" [Already you have a user account]
+*/
 
-
+/*
+    Get form value:
+    ------------------ 
+    1. Set form onSubmitHandler
+    2. Set Event Parameter
+    3. event.target.email.value
 */
