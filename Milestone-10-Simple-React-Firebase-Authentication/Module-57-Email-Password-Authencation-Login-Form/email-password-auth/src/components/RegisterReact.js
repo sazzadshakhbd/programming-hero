bea8cb8a8a2fa37a49930393fb043488cb31e461
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-18 21:39:20 +0600

    Create new user using email and password on firebase

--- Milestone-10-Simple-React-Firebase-Authentication/Module-57-Email-Password-Authencation-Login-Form/email-password-auth/src/components/RegisterReact.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-57-Email-Password-Authencation-Login-Form/email-password-auth/src/components/RegisterReact.js
@@ -1,10 +1,22 @@
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
+import app from '../firebase/firebase.init';
+
+const auth = getAuth(app);
 
 const handelOnSubmit = (event) => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => {
+            console.log('error', error);
+        })
     console.log(email, password);
 }
 
@@ -14,12 +26,12 @@ const RegisterReact = () => {
             <Form className='my-5' onSubmit={handelOnSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                    <Form.Control type="email" name="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="Password" />
+                    <Form.Control type="password" name="password" placeholder="Password" required />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
