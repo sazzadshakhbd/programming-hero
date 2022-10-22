808712a26c05bcc99b7beced8248f5802a7c92a3
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-22 22:12:25 +0600

    Get Form Data and display form error

--- Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/components/Signup/Signup.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/components/Signup/Signup.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Signup.css'
 
 const Signup = () => {
+    const [error, setError] = useState(null)
+    const handelSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email, password, confirm);
+
+        if (password.length < 6) {
+            setError('Password should be 6 characters or more');
+            return;
+        }
+        if (password !== confirm) {
+            setError('Your password did not match')
+            return
+        }
+    }
     return (
         <div className='form-container'>
             <h2 className='form-title'>Sign Up</h2>
-            <form >
+            <form onSubmit={handelSubmit}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" required />
@@ -22,7 +40,7 @@ const Signup = () => {
                 <input className='btn-submit' type="submit" value="Sign Up" />
             </form>
             <p>Already Have an Account <Link to='/login'>Login</Link></p>
-
+            <p className='text-error'>{error}</p>
         </div>
     );
 };
--- Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/contexts/UserContext.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/contexts/UserContext.js
@@ -1,8 +1,13 @@
-import React, { createContext } from 'react';
-const AuthContext = createContext();
+import React, { createContext, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import app from '../firebase/firebase.config';
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
 
 const UserContext = ({ children }) => {
-    const authInfo = {}
+    const [user, setUser] = useState(null)
+    const authInfo = { user }
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
