f593dc1da58807de7346d5b4772d32554cf92210
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-22 19:17:27 +0600

    Module Introduction firebase setup and git clone

--- Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/App.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/App.js
@@ -4,6 +4,8 @@ import About from './components/About/About';
 import Main from './layouts/Main';
 import Shop from './components/Shop/Shop';
 import Orders from './components/Orders/Orders';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 import Inventory from './components/Inventory/Inventory';
 import { productsAndCartLoader } from './loaders/productsAndCartLoader';
 
@@ -11,7 +13,7 @@ import { productsAndCartLoader } from './loaders/productsAndCartLoader';
 function App() {
   const router = createBrowserRouter([
     {
-      path:'/',
+      path: '/',
       element: <Main></Main>,
       children: [
         {
@@ -20,7 +22,7 @@ function App() {
           element: <Shop></Shop>
         },
         {
-          path:'orders',
+          path: 'orders',
           loader: productsAndCartLoader,
           element: <Orders></Orders>
         },
@@ -29,12 +31,20 @@ function App() {
           element: <Inventory></Inventory>
         },
         {
-          path:'about',
-          element:<About></About>
+          path: 'about',
+          element: <About></About>
+        },
+        {
+          path: 'login',
+          element: <Login></Login>
+        },
+        {
+          path: 'signup',
+          element: <Signup></Signup>
         }
       ]
     },
-    
+
   ])
   return (
     <div>
---
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDrEpTTYFfd5wSyR0o5y_tT9ZoLFXG8WJc",
+    authDomain: "ema-john-simple-4630d.firebaseapp.com",
+    projectId: "ema-john-simple-4630d",
+    storageBucket: "ema-john-simple-4630d.appspot.com",
+    messagingSenderId: "850167001725",
+    appId: "1:850167001725:web:31fe2e290fe1ef7f9a7714"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
