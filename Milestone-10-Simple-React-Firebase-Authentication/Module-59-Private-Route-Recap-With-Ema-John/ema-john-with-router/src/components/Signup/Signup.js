3d4c77259d26870cddb5896163a086eee60ae557
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-22 21:37:44 +0600

    Setup Auth Context under UserContext to share auth info

--- Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/components/Signup/Signup.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/components/Signup/Signup.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Signup.css'
 
 const Signup = () => {
     return (
-        <div>
-            <h2>SignUp Page</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <form >
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirm">Confirm Password</label>
+                    <input type="password" name="confirm" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Sign Up" />
+            </form>
+            <p>Already Have an Account <Link to='/login'>Login</Link></p>
+
         </div>
     );
 };
