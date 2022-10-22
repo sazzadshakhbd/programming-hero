9c565184723306da29d6556e261dfee7e28a1be1
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-22 10:00:58 +0600

    Create User Context for shared authentication

---
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-58-React-Auth-Integration-And-Private-Route/auth-router-context-app/src/contexts/UserContext.js
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react';
+const AuthContext = createContext();
+
+const UserContext = ({ children }) => {
+    const user = { displayName: 'Sazzad' }
+    const authInfo = { user: user }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default UserContext;
---
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-58-React-Auth-Integration-And-Private-Route/auth-router-context-app/src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBLjY3ZDMczSn3aMN4XSLEGj1rM57WZoCQ",
+    authDomain: "auth-router-context-5f098.firebaseapp.com",
+    projectId: "auth-router-context-5f098",
+    storageBucket: "auth-router-context-5f098.appspot.com",
+    messagingSenderId: "311451289379",
+    appId: "1:311451289379:web:78ee293db09e80b2586e96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
