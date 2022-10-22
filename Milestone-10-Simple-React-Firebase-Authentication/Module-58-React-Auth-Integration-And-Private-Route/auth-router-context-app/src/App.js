43842394614d91d2618b31816261e299ad02718c
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-22 07:53:27 +0600

    Module Introduction and Basic Project Setup

--- Milestone-10-Simple-React-Firebase-Authentication/Module-58-React-Auth-Integration-And-Private-Route/auth-router-context-app/src/App.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-58-React-Auth-Integration-And-Private-Route/auth-router-context-app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Registration from './components/Registration';
+import Main from './layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>,
+        },
+        {
+          path: '/login',
+          element: <Login></Login>,
+        },
+        {
+          path: '/registration',
+          element: <Registration></Registration>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-58-React-Auth-Integration-And-Private-Route/auth-router-context-app/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="navbar bg-primary text-primary-content">
+            <Link className="btn btn-ghost normal-case text-xl" to='/'>daisyUI</Link>
+            <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
+            <Link className="btn btn-ghost normal-case text-xl" to='/login'>Login</Link>
+            <Link className="btn btn-ghost normal-case text-xl" to='/registration'>Registration</Link>
+        </div>
+    );
+};
+
+export default Header;
