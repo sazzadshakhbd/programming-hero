2d5cb9cfecfde06eea1df87145f670d1ea4a4661
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-23 10:27:26 +0600

    Implement Login Sign Up and Sign Out

--- Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/components/Signup/Signup.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/components/Signup/Signup.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 import './Signup.css'
 
 const Signup = () => {
     const [error, setError] = useState(null)
+    const { createUser } = useContext(AuthContext)
     const handelSubmit = (event) => {
         event.preventDefault();
         const form = event.target;
@@ -20,6 +22,15 @@ const Signup = () => {
             setError('Your password did not match')
             return
         }
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset()
+            })
+            .catch(error => {
+                console.error('error', error)
+            })
     }
     return (
         <div className='form-container'>
