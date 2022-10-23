f7145936284ba6b20d206096c4bed97e15ecfe84
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-23 12:15:59 +0600

    firbase deploy done

--- Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/components/Inventory/Inventory.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-59-Private-Route-Recap-With-Ema-John/ema-john-with-router/src/components/Inventory/Inventory.js
@@ -4,6 +4,7 @@ const Inventory = () => {
     return (
         <div>
             <h2>This is Inventory</h2>
+            <h3>It's a very import page for this project</h3>
         </div>
     );
 };
