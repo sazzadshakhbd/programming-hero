544dc6f13a235116c5ba0c1424b273ad3c4a9bce
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-23 20:30:19 +0600

    Dragon-News-Client NavBar with responsive content toggle

--- Milestone-10-Simple-React-Firebase-Authentication/Module-60-Recap-With-A-Responisve-React-Website/dragon-news-client/src/layout/Main.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-60-Recap-With-A-Responisve-React-Website/dragon-news-client/src/layout/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Header from '../Pages/Shared/Header/Header';
@@ -12,21 +12,18 @@ const Main = () => {
             <Header></Header>
             <Container>
                 <Row>
-                    <Col lg={2}>
+                    <Col lg="2" className='d-none d-lg-block'>
                         <LeftSideNav></LeftSideNav>
                     </Col>
-                    <Col lg={7}>
-
+                    <Col lg="7">
                         <Outlet></Outlet>
                     </Col>
-                    <Col lg={3}>
+                    <Col lg="3">
                         <RightSideNav></RightSideNav>
                     </Col>
                 </Row>
             </Container>
-
             <Footer></Footer>
-
         </div>
     );
 };
