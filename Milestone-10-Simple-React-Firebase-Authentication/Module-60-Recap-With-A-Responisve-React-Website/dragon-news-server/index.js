21d5cd0ff9caa580f9dcc6a4a0ee95112ddce60e
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-10-24 11:04:56 +0600

    Dragon-News-Client Create New and category data loading API

--- Milestone-10-Simple-React-Firebase-Authentication/Module-60-Recap-With-A-Responisve-React-Website/dragon-news-server/index.js
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-60-Recap-With-A-Responisve-React-Website/dragon-news-server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running')
@@ -13,6 +14,26 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+});
+
+app.get('/news/:id', (req, res) => {
+    // console.log(req.params.id);
+    const id = req.params.id;
+    const selectNews = news.find(n => n._id === id);
+    res.send(selectNews);
+});
+
+app.get('/category/:id', (req, res) => {
+    // console.log(req.params.category_id)
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews)
+    }
+
 })
 
 app.listen(port, () => {
