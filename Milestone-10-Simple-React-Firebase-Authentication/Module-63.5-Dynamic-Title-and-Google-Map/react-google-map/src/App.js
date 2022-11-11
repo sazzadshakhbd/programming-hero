66a22edaaf2c86b9efbff1dabbb2744bd821642d
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-11-11 08:25:31 +0600

    modile 63.5 maps implemention done

---
+++ Milestone-10-Simple-React-Firebase-Authentication/Module-63.5-Dynamic-Title-and-Google-Map/react-google-map/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyLocation from './components/myLocation';
+
+function App() {
+  return (
+    <>
+      <MyLocation></MyLocation>
+    </>
+  );
+}
+
+export default App;
